6a2651ab1a28fd222f7efb95f90c96057a107a01
aamine <aamine@1b9489fe-b721-0410-924e-b54b9192deb8>
2000-03-22 08:24:21 +0000

    add sample/* into main trunk
    git-svn-id: file:///Users/aaron/Desktop/public/racc/trunk@1693 1b9489fe-b721-0410-924e-b54b9192deb8

---
+++ sample/compile.rb
@@ -0,0 +1,52 @@
+#!/usr/local/bin/ruby
+
+require 'amstd/rbparams'
+require 'amstd/inst'
+
+
+rubypath = RubyParams::RUBY_PATH
+raccpath = './bin/racc'
+$cmd     = "ruby #{raccpath} -ocalc.rb -e#{rubypath} %s"
+
+
+def compile( file )
+  if system sprintf( $cmd, file ) then
+    print <<S
+
+calc.rb successfuly compiled.
+to execute, type "./calc.rb" on command line.
+
+S
+  else
+    print <<S
+
+compile failed!!!
+please bug report to me <aamine@dp.u-netsurf.ne.jp>
+
+S
+  end
+end
+
+
+def usage( st )
+  print <<S
+
+usage:
+
+  ruby compile.rb [calc|calc2]
+
+S
+  exit st
+end
+
+
+case ARGV[0]
+when nil, 'calc', 'calc1'
+  compile 'calc.y'
+when 'calc2', 'jcalc'
+  compile 'calc2-ja.y'
+when '--help', '-h', '--version', '-v'
+  usage 0
+else
+  usage 1
+end
