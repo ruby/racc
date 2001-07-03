750a3ccddca8fa28b6ece4c4b7a94deb0ffed977
aamine <aamine@1b9489fe-b721-0410-924e-b54b9192deb8>
2001-07-03 11:50:56 +0000

    o  pack.rb:  convert to new remake format (set/get.vername)
    git-svn-id: file:///Users/aaron/Desktop/public/racc/trunk@1929 1b9489fe-b721-0410-924e-b54b9192deb8

--- pack.rb
+++ pack.rb
@@ -8,38 +8,38 @@ require 'amstd/pack'
 
 environ( 'racc' ) do
 
-  set :version, '1.3.9'
+  set.version   = '1.3.9'
 
-  set :dir, expand('~/r/racc')
+  set.dir       = expand('~/r/racc')
 
-  set :type,      'ruby'
-  set :package,   'racc'
-  set :instpath,  'racc'
-  set :topfile,    nil
-  set :raw,        true
+  set.type      = 'ruby'
+  set.package   = 'racc'
+  set.instpath  = 'racc'
+  set.topfile   =  nil
+  set.raw       =  true
 
-  set :extern_libs, %w( raccrt strscan amstd )
+  set.extern_libs = %w( raccrt strscan amstd )
 
-  set :view_name, 'Racc'
-  set :category,  'parser generator'
-  set :format,    'ruby script, ruby extention'
-  set :require,   'ruby(>=1.4), C compiler'
-  set :license,   'LGPL'
-  set :manual,     true
+  set.view_name = 'Racc'
+  set.category  = 'parser generator'
+  set.format    = 'ruby script, ruby extention'
+  set.require   = 'ruby(>=1.4), C compiler'
+  set.license   = 'LGPL'
+  set.manual    =  true
 
-  set :description_ja, <<'----'
+  set.description_ja = <<'----'
 Ruby 用のパーザジェネレータ (yacc みたいなの) です。
 Ruby スクリプトでは最速のパーザを生成します。
 ----
-  set :description_en, <<'----'
+  set.description_en = <<'----'
 Racc (Ruby yACC) is a LALR(1) Parser Generator for Ruby.
 This tool is written in Ruby and outputs Ruby scripts.
 ----
 
 
-  set :bin, %w( racc )
+  set.bin = %w( racc )
 
-  set :mainrb, %w(
+  set.mainrb = %w(
     compiler.rb
     grammer.rb
     info.rb
@@ -51,23 +51,23 @@ This tool is written in Ruby and outputs Ruby scripts.
     ucodep.rb
   )
 
-  set :src, %w(
+  set.src = %w(
     build
   )
-  set :genrb, %w( raccp.rb )
+  set.genrb = %w( raccp.rb )
 
-  set :rb, (g :mainrb) + (g :genrb)
+  set.rb = get.mainrb + get.genrb
 
-  set :tool, %w(
+  set.tool = %w(
     pack.rb
   )
 
-  set :misc, %w(
+  set.misc = %w(
     y2racc
     racc2y
   )
 
-  set :text, %w(
+  set.text = %w(
     FILES
     BUGS.ja
     BUGS.en
@@ -75,14 +75,14 @@ This tool is written in Ruby and outputs Ruby scripts.
 
 
   def build
-    upver a(g :src)
-    chdir( g :dir ) do
+    upver a(get.src)
+    chdir( get.dir ) do
       command './build &> er'
     end
   end
 
   def update
-    upver a(g(:bin), g(:mainrb), g(:src))
+    upver a(get.bin, get.mainrb, get.src)
     (e :raccrt).update
   end
 
@@ -111,17 +111,17 @@ end
 
 environ( 'raccrt' ) do
 
-  set :version, (e :racc).g(:version)
-  set :dir, (e :racc).g(:dir)
+  set.version     = (e :racc).get.version
+  set.dir         = (e :racc).get.dir
 
-  set :type,     'ruby'
-  set :package,  'raccrt'
-  set :instpath, 'racc'
-  set :topfile,   nil
+  set.type        = 'ruby'
+  set.package     = 'raccrt'
+  set.instpath    = 'racc'
+  set.topfile     =  nil
 
-  set :intern_libs, %w( cparse )
+  set.intern_libs = %w( cparse )
 
-  set :rb, %w( parser.rb )
+  set.rb = %w( parser.rb )
 
   def update
     upver a(g :rb), (e :racc).g(:version)
@@ -135,16 +135,16 @@ environ( 'raccrt' ) do
 
   environ( 'cparse' ) do
 
-    set :version, (e :racc).g(:version)
-    set :dir, expand('~/r/racc/cp')
+    set.version  = (e :racc).g(:version)
+    set.dir      = expand('~/r/racc/cp')
 
-    set :type,     'ext'
-    set :package,  'raccrt'
-    set :instpath, 'racc'
-    set :topfile,   nil
+    set.type     = 'ext'
+    set.package  = 'raccrt'
+    set.instpath = 'racc'
+    set.topfile  =  nil
 
-    set :c, %w( cparse.c )
-    set :etool, %w( MANIFEST extconf.rb )
+    set.c        = %w( cparse.c )
+    set.etool    = %w( MANIFEST extconf.rb )
 
     def update
       upver a(g :c), (e :racc).g(:version)
