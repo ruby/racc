50229d19edd9e681058a26ce3d32b603eb5601a0
aamine <aamine@1b9489fe-b721-0410-924e-b54b9192deb8>
2003-11-03 08:19:36 +0000

    * lib/racc/parser.rb: update version. * ext/racc/cparse/cparse.c: update version.
    git-svn-id: file:///Users/aaron/Desktop/public/racc/trunk@2083 1b9489fe-b721-0410-924e-b54b9192deb8

--- ext/racc/cparse/cparse.c
+++ ext/racc/cparse/cparse.c
@@ -20,7 +20,7 @@
                         Important Constants
 ----------------------------------------------------------------------- */
 
-#define RACC_VERSION "1.4.3"
+#define RACC_VERSION "1.4.4"
 
 #define DEFAULT_TOKEN -1
 #define ERROR_TOKEN    1
