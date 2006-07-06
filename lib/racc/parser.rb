68569380438fb69e73869c9695ec76659224ee56
aamine <aamine@1b9489fe-b721-0410-924e-b54b9192deb8>
2006-07-06 11:42:07 +0000

    update copyright year.
    git-svn-id: file:///Users/aaron/Desktop/public/racc/trunk@2148 1b9489fe-b721-0410-924e-b54b9192deb8

--- lib/racc/parser.rb
+++ lib/racc/parser.rb
@@ -1,7 +1,7 @@
 #
 # $Id$
 #
-# Copyright (c) 1999-2005 Minero Aoki
+# Copyright (c) 1999-2006 Minero Aoki
 #
 # This program is free software.
 # You can distribute/modify this program under the same terms of ruby.
