abfdc3fe89113f311d46e2474daf21152a411417
Aaron Patterson <aaron.patterson@gmail.com>
2008-12-21 18:47:34 -0800

    updating documentation

--- tasks/doc.rb
+++ tasks/doc.rb
@@ -1,8 +1,10 @@
 require 'rake/rdoctask'
 
 Rake::RDocTask.new(:docs) do |rd|
-  rd.main = "README.en"
-  rd.rdoc_files.include(SPEC.files)
+  rd.main = "README.en.rdoc"
+  rd.rdoc_files.include(SPEC.files.find_all { |file_name|
+    file_name =~ /^(bin|lib|ext)/ || file_name !~ /\//
+  })
 
   title = "#{SPEC.name}-#{SPEC.version} Documentation"
 
