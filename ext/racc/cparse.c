877f11826494355ffdbb47d5b0bac39db72a892a
Aaron Patterson <aaron.patterson@gmail.com>
2013-10-29 11:36:07 -0700

    bumping version

--- ext/racc/cparse.c
+++ ext/racc/cparse.c
@@ -17,7 +17,7 @@
                         Important Constants
 ----------------------------------------------------------------------- */
 
-#define RACC_VERSION "1.4.10"
+#define RACC_VERSION "1.4.11"
 
 #define DEFAULT_TOKEN -1
 #define ERROR_TOKEN    1
