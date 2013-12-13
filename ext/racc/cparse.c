7fc11c668d7d2fdc9815248b1c41ce40765b9881
Aaron Patterson <aaron.patterson@gmail.com>
2013-12-13 14:56:13 -0800

    We can't rename this because racc will rescue load errors and use the pure ruby runtime.
    Revert "Renamed Init_cparse to Init_racc for 1.8 loader"
    
    This reverts commit d59a60ff9c1d6f9d50c111590fb035aa348909f3.

--- ext/racc/cparse.c
+++ ext/racc/cparse.c
@@ -789,7 +789,7 @@ reduce0(VALUE val, VALUE data, VALUE self)
 ----------------------------------------------------------------------- */
 
 void
-Init_racc(void)
+Init_cparse(void)
 {
     VALUE Racc, Parser;
     ID id_racc = rb_intern("Racc");
