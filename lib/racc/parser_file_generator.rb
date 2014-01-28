a87b5ccb473f32ef0da805913f489025e139cdf7
Sylvester Keil <sylvester@keil.or.at>
2014-01-28 12:17:05 +0100

    Fix module "end" indentation
    Fix the indentation of the closing end statements of modules
    in the generated parser file.

--- lib/racc/parser_file_generator.rb
+++ lib/racc/parser_file_generator.rb
@@ -154,8 +154,8 @@ module Racc
       cref_pop
       indent; line "end   \# class #{classid}"
       mods.reverse_each do |mod|
-        indent; line "end   \# module #{mod}"
         cref_pop
+        indent; line "end   \# module #{mod}"
       end
     end
 
