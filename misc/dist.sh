df4e05361dad93fafcf82fb564446cb28b7953f7
Alex Dowad <alexinbeijing@gmail.com>
2015-11-13 19:01:33 +0200

    Remove setup.rb
    Nowadays everybody (as in, EVERYBODY) uses Rubygems. This install script was
    useful in the past, but is now an anachronism.

--- misc/dist.sh
+++ misc/dist.sh
@@ -13,7 +13,6 @@ cd ..
 mkdir -p raccrt-$version/lib/racc
 mv racc-$version/lib/racc/parser.rb     raccrt-$version/lib/racc
 mv racc-$version/ext                    raccrt-$version
-cp racc-$version/setup.rb               raccrt-$version
 cp racc-$version/README.*               raccrt-$version
 cp racc-$version/COPYING                raccrt-$version
 tar czf $ardir/racc/racc-$version.tar.gz racc-$version
@@ -21,7 +20,6 @@ tar czf $ardir/raccrt/raccrt-$version.tar.gz raccrt-$version
 
 # -all
 mkdir -p racc-$version-all/packages
-cp racc-$version/setup.rb racc-$version-all
 cp racc-$version/README.* racc-$version-all
 mv racc-$version          racc-$version-all/packages/racc
 mv raccrt-$version        racc-$version-all/packages/raccrt
