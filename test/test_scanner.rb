cd4ddc2056465c12997499939bc109e957603bf9
Alex Dowad <alexinbeijing@gmail.com>
2015-11-18 22:21:13 +0200

    Convert scanner test script into a MiniTest test suite

---
+++ test/test_scanner.rb
@@ -0,0 +1,26 @@
+require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
+
+# The complicated part of the grammar file scanner is dealing with Ruby code
+# blocks, so that is what we focus on testing
+
+module Racc
+  class TestScanner < TestCase
+    Dir[File.join(File.dirname(__FILE__), 'scandata', '*')].each do |testfile|
+      define_method("test_scan_#{File.basename(testfile)}".to_sym) do
+        original = File.read(testfile)
+        # wrap the Ruby source code in an action block
+        wrapped  = "class Test\nrule\na : '*' {" << original << "\n}"
+        scanner  = Racc::GrammarFileScanner.new(wrapped, testfile)
+
+        rebuilt = ''
+        scanner.yylex do |token|
+          break if token.nil?
+          rebuilt << token[1].text if token[0] == :ACTION
+        end
+        rebuilt.chomp!("\n")
+
+        assert_equal original, rebuilt
+      end
+    end
+  end
+end
