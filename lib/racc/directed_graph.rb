aa5c25f4d83f4178777a94ed3442ac43c5a1741b
Alex Dowad <alexinbeijing@gmail.com>
2015-12-04 10:27:33 +0200

    Add Graph::Labeled

--- lib/racc/directed_graph.rb
+++ lib/racc/directed_graph.rb
@@ -186,6 +186,115 @@ module Racc
       end
     end
 
+    # Each vector has a label; labels are unique for any source node
+    # There can be multiple vectors from one node to another, as long as
+    # each vector has a different label
+    class Labeled < Array
+      include Algorithms
+
+      def initialize(size)
+        super(size) { {} }
+        concat(map { Set.new }) # backpointers
+        @start  = nil
+        @offset = size
+      end
+
+      attr_reader :start
+
+      def start=(idx)
+        @start = idx
+      end
+
+      def add_vector(from, to, label)
+        if self[from].key?(label)
+          raise "Vector #{label.inspect} from node #{from} already exists"
+        end
+        self[from][label] = to
+        self[to + @offset] << from
+        to
+      end
+
+      def remove_vector(from, to, label)
+        self[from].delete(label)
+        self[to + @offset].delete(from)
+      end
+
+      def nodes(&block)
+        result = 0...@offset
+        result.each(&block) if block_given?
+        result
+      end
+
+      def children(node, &block)
+        result = self[node].values
+        result.each(&block) if block_given?
+        result
+      end
+
+      def vectors(node, &block)
+        result = self[node]
+        result.each(&block) if block_given?
+        result
+      end
+
+      def parents(node, &block)
+        result = self[@offset + node]
+        result.each(&block) if block_given?
+        result
+      end
+
+      def reachable
+        reachable_from([@start])
+      end
+
+      def reachable_from(nodes)
+        Racc.set_closure(nodes) { |node| self[node].values }
+      end
+
+      def leaves
+        reachable.select { |node| self[node].empty? }
+      end
+
+      # like #all_paths, but return sequences of vector labels, not sequences
+      # of nodes
+      def all_vector_paths(src, dest, current=src, kill=0, path=[], result=[])
+        vectors(current) do |label, child|
+          if child == dest
+            result << (path.dup << label)
+          elsif kill[child] == 0
+            path.push(label)
+            all_vector_paths(src, dest, child, kill | (1 << child), path, result)
+            path.pop
+          end
+        end
+        result
+      end
+
+      # like #shortest_paths, but return sequences of vector labels
+      def shortest_vector_paths
+        paths = {@start => [0]} # cache total path cost
+
+        Racc.set_closure([@start]) do |node|
+          node_path = paths[node]
+          vectors(node) do |label, child|
+            child_path = paths[child]
+            cost = block_given? ? yield(node, child) : 1
+            if child_path.nil? || child_path[0] > node_path[0] + cost
+              paths[child] = node_path.dup.tap { |p| p[0] += cost } << label
+            end
+          end
+          children(node)
+        end
+
+        paths.each_value { |p| p.shift } # drop cached total costs
+        paths
+      end
+
+      def dup
+        super.map!(&:dup)
+      end
+    end
+
     # This implementation uses an object for each node, rather than identifying
     # nodes by integers
     # this means we can add as many nodes as we want
