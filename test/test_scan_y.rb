44c5b4e6b0624227b94bd2d7193fa7cc1d45a92e
Keiji, Yoshimi <walf443@gmail.com>
2016-01-24 03:27:44 +0900

    fixed rubocop styles of test/test_scan_y.rb

--- test/test_scan_y.rb
+++ test/test_scan_y.rb
@@ -9,6 +9,15 @@ module Racc
       @states = @result.grammar.states
     end
 
+    def assert_not_conflict(states)
+      grammar = states.grammar
+
+      assert_equal 0, states.sr_conflicts.size
+      assert_equal 0, states.rr_conflicts.size
+      assert_equal 0, grammar.useless_symbols.size
+      assert_nil grammar.n_expected_srconflicts
+    end
+
     def test_compile
       generator = Racc::ParserFileGenerator.new(@states, @result.params.dup)
 
@@ -17,12 +26,7 @@ module Racc
         class_eval(generator.generate_parser)
       }
 
-      grammar = @states.grammar
-
-      assert_equal 0, @states.sr_conflicts.size
-      assert_equal 0, @states.rr_conflicts.size
-      assert_equal 0, grammar.useless_symbols.size
-      assert_nil grammar.n_expected_srconflicts
+      assert_not_conflict(@states)
     end
   end
 end
