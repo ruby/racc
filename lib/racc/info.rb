7bdd36ed974cfce6c267996af150b7c17c9c162d
Ryan Davis <ryand-ruby@zenspider.com>
2017-05-10 13:31:05 -0700

    Fix VERSION string to be compliant to rubygems spec. Removes hoe warning.

--- lib/racc/info.rb
+++ lib/racc/info.rb
@@ -6,7 +6,7 @@
 # For details of the GNU LGPL, see the file "COPYING".
 
 module Racc
-  VERSION   = '2.0.0-dev'
+  VERSION   = "2.0.0.dev"
   CODENAME  = 'Mecha Oishii'
   COPYRIGHT = 'Copyright (c) 1999-2006 Minero Aoki'
 end
