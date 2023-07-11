e42bf8f14f6c1175f964c7118a8ec81d6a2dcbd8
Johnny Willemsen <jwillemsen@remedy.nl>
2023-07-11 17:38:16 +0200

    Update parser.rb, fixed typo

--- lib/racc/parser.rb
+++ lib/racc/parser.rb
@@ -53,7 +53,7 @@ end
 # [-v, --verbose]
 #   verbose mode. create +filename+.output file, like yacc's y.output file.
 # [-g, --debug]
-#   add debug code to parser class. To display debuggin information,
+#   add debug code to parser class. To display debugging information,
 #   use this '-g' option and set @yydebug true in parser class.
 # [-E, --embedded]
 #   Output parser which doesn't need runtime files (racc/parser.rb).
